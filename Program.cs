6b39ada71f637e75735171ed73959c5898555ede
NeigeDayor <descompteslecompte@gmail.com>
2026-03-16 17:10:19 +0100

    Modification Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-﻿(int, int) ConvertNbMinToHM(int nbMin)
-{
+﻿(int, int) ConvertNbMinToHM(int nbMin){
 	return (nbMin/60, nbMin%60);
 }
 
-(int H, int M) = ConvertNbMinToHM(130);
-
+int nbMin;
+Console.WriteLine("Saisir le nombre de minutes : ");
+nbMin = Convert.ToInt32(Console.ReadLine());
+(int H, int M) = ConvertNbMinToHM(nbMin);
 Console.WriteLine($"{H}:{M}");
